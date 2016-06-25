e65511e2093fdaa9c3a1f26d7fab7b723383222a
Piotr Król <piotr.krol@3mdeb.com>
2016-06-26 00:41:07 +0200

    init.sh: handle docker build errors

--- init.sh
+++ init.sh
@@ -7,5 +7,11 @@ if [ ! -d  ../lib ]; then
 fi
 
 docker build -t 3mdeb/armbian .
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to build container"
+    exit 1
+fi
+
 cd ..
 docker run -v ${PWD}/lib:/root/lib -t -i 3mdeb/armbian
